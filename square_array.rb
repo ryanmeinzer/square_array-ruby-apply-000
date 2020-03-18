b9d97c33f8d433dd541c950352b40282b3b6b00d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-03-18 19:21:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@
 def square_array(array)
   squared_array = []
   array.each do |each_element|
-    squared_array << each_element.to_i * each_element.to_i
-    # squared_array << each_element ** 2
+    # squared_array << each_element.to_i * each_element.to_i
+    squared_array << each_element ** 2
   end
   squared_array
 end
